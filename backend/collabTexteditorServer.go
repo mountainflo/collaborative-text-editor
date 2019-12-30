1dc1f6577bd52ef5256e97ef8a98514cfcbdf84d
mountainflo <tobusch@hm.edu>
2019-12-30 16:56:44 +0100

    #2 go server stores text over requests

--- backend/collabTexteditorServer.go
+++ backend/collabTexteditorServer.go
@@ -13,49 +13,52 @@ const (
 	port = ":9090"
 )
 
-//TODO if not possible to store subscriberStreams, use a simple blob-store also connected via gRPC
-type server struct {
-	pb.UnimplementedCollabTexteditorServiceServer
-	subscriberStreams []pb.CollabTexteditorService_SubscribeToServerUpdateServer
-	savedText         string
+//TODO if not possible to store SubscriberStreams, use a simple blob-store also connected via gRPC
+type collabTexteditorService struct {
+	repository *Repository
+}
+
+type Repository struct {
+	SavedText string
+	Streams   []*pb.CollabTexteditorService_SubscribeToServerUpdateServer
 }
 
 //TODO process TextUpdateRequest and send back a status message
-func (s server) SendTextUpdate(ctx context.Context, request *pb.TextUpdateRequest) (*pb.TextUpdateReply, error) {
+func (c collabTexteditorService) SendTextUpdate(ctx context.Context, request *pb.TextUpdateRequest) (*pb.TextUpdateReply, error) {
 
-	s.savedText = request.TextUpdate
+	c.repository.SavedText = request.TextUpdate
 
-	log.Println("Server received test update: " + s.savedText)
+	log.Println("Server received test update: " + c.repository.SavedText)
 
-	s.sendUpdateToSubscribers()
+	c.sendUpdateToSubscribers()
 
 	return &pb.TextUpdateReply{StatusMessage: "Successfully received text update"}, nil
 }
 
-//TODO save client id and if server receives an update by SendTextUpdate, then stream the update
-func (s server) SubscribeToServerUpdate(request *pb.ServerUpdateSubscriptionRequest, stream pb.CollabTexteditorService_SubscribeToServerUpdateServer) error {
+//TODO save client id and if collabTexteditorService receives an update by SendTextUpdate, then stream the update
+func (c collabTexteditorService) SubscribeToServerUpdate(request *pb.ServerUpdateSubscriptionRequest, stream pb.CollabTexteditorService_SubscribeToServerUpdateServer) error {
 
 	log.Println("Client " + strconv.Itoa(int(request.ClientId)) + " subscribes for updates")
 
-	s.subscriberStreams = append(s.subscriberStreams, stream)
+	c.repository.Streams = append(c.repository.Streams, &stream)
 
 	//return nil //end the stream
 	return nil
 }
 
-func (s server) sendUpdateToSubscribers() {
+func (c collabTexteditorService) sendUpdateToSubscribers() {
 
 	log.Println("send update to subscribers")
 
-	response := pb.ServerUpdateSubscriptionResponse{LatestServerContent: s.savedText}
+	response := pb.ServerUpdateSubscriptionResponse{LatestServerContent: c.repository.SavedText}
 
 	counter := 0
 
-	for _, stream := range s.subscriberStreams {
+	for _, stream := range c.repository.Streams {
 
 		log.Println("send update to subscriber: " + strconv.Itoa(counter))
 
-		if err := stream.Send(&response); err != nil {
+		if err := (*stream).Send(&response); err != nil {
 			log.Printf(err.Error())
 		}
 
@@ -65,15 +68,19 @@ func (s server) sendUpdateToSubscribers() {
 }
 
 func main() {
+
+	textStorage := &Repository{}
+	repository := &collabTexteditorService{textStorage}
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	// Creates a new gRPC server
+	// Creates a new gRPC collabTexteditorService
 	s := grpc.NewServer()
 
-	pb.RegisterCollabTexteditorServiceServer(s, &server{})
+	pb.RegisterCollabTexteditorServiceServer(s, repository)
 	s.Serve(lis)
 
-	//TODO close subscriberStreams before shutdown (using signals)
+	//TODO close SubscriberStreams before shutdown (using signals and defer)
 }
