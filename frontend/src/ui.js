2e31354ed33807d189bb26d2c2aeee2acdc95432
mountainflo <tobusch@hm.edu>
2020-02-08 14:22:36 +0100

    #10 improve ui

--- frontend/src/ui.js
+++ frontend/src/ui.js
@@ -21,21 +21,36 @@ function sessionSelectionButtonListener(callback, textAreaObj) {
 }
 
 function joinSessionButtonListener(callback, textAreaObj) {
-  document.getElementById('joinSessionButton').addEventListener('click', function() {
+  const joinSessionButton = document.getElementById('joinSessionButton');
+  joinSessionButton.addEventListener('click', function() {
     // TODO check that the input fields are not empty
     const nickName = document.getElementById('joinSessionNickName').value;
     const sessionId = document.getElementById(SESSION_ID_INPUT).value;
     switchVisibilityOfElementsById('joinSession', EDITOR);
-
     callback(textAreaObj, nickName, sessionId);
+  });
+  const inputField = document.getElementById('joinSessionNickName');
+  inputField.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      joinSessionButton.click();
+    }
   });
 }
 
 function createSessionButtonListener(callback, textAreaObj) {
-  document.getElementById('createSessionButton').addEventListener('click', function() {
+  const createSessionButton = document.getElementById('createSessionButton');
+  createSessionButton.addEventListener('click', function() {
     const nickName = document.getElementById('createSessionNickName').value;
     switchVisibilityOfElementsById('createSession', EDITOR);
     callback(textAreaObj, nickName);
+  });
+  const inputField = document.getElementById('createSessionNickName');
+  inputField.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      createSessionButton.click();
+    }
   });
 }
 
